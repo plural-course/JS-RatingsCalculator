7f0568ad315b2b3f394740ad3c49bd71b3751b60
sdtest <sdtest@gmail.com>
2021-05-26 15:08:08 +0200

    excercises finished

---
+++ js/ratings.js
@@ -0,0 +1,25 @@
+function collect_ratings () {
+    const elements = document.querySelectorAll(".rating");
+    var ratings = {
+        count:0,
+        sum:0,
+        average:0
+    }
+    let rating = 0;
+    elements.forEach((element) => {
+        let temp = element.id.replace('star', '');
+        //element.id = parseInt(temp);
+        rating = parseInt(temp);
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
+    });
+    if(ratings.count != 0){
+        ratings.average = ratings.sum/ratings.count;
+    }
+    return ratings;
+}
+
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    document.querySelector("#average").value = parseFloat(ratings.average).toFixed(2);
+});
